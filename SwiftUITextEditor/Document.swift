81f4ba4f920344c288239db0f4e5d8eda8556afe
Mark Szymczyk <mark@meandmark.com>
2019-12-19 15:59:23 -0500

    Add code to save and load documents.

--- SwiftUITextEditor/Document.swift
+++ SwiftUITextEditor/Document.swift
@@ -13,11 +13,14 @@ class Document: UIDocument {
     
     override func contents(forType typeName: String) throws -> Any {
         // Encode your document with an instance of NSData or NSFileWrapper
-        return Data()
+        return try NSKeyedArchiver.archivedData(withRootObject: text, requiringSecureCoding: true)
     }
     
     override func load(fromContents contents: Any, ofType typeName: String?) throws {
         // Load your document from contents
+        guard let data = contents as? Data else { return }
+        guard let fileContents = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? String else { return }
+        text = fileContents
     }
 }
 
