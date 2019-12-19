01a14b4d4eb314bf57296ec5aeb8b2f29cdd6e69
Mark Szymczyk <mark@meandmark.com>
2019-12-18 22:22:26 -0500

    Give the document a text property that holds the contents of the text file.

--- SwiftUITextEditor/Document.swift
+++ SwiftUITextEditor/Document.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class Document: UIDocument {
+    var text = ""
     
     override func contents(forType typeName: String) throws -> Any {
         // Encode your document with an instance of NSData or NSFileWrapper
