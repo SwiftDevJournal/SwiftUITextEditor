b42bd12a93f2f0e7fea624b2705af68695c4faf2
Mark Szymczyk <mark@meandmark.com>
2019-12-18 22:26:33 -0500

    Add code to load the empty text file when creating a new document.

--- SwiftUITextEditor/DocumentBrowserViewController.swift
+++ SwiftUITextEditor/DocumentBrowserViewController.swift
@@ -32,12 +32,13 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
     // MARK: UIDocumentBrowserViewControllerDelegate
     
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
-        let newDocumentURL: URL? = nil
+        let newDocumentURL: URL? = Bundle.main.url(forResource: "New Document",
+        withExtension: "txt")
         
         // Set the URL for the new document here. Optionally, you can present a template chooser before calling the importHandler.
         // Make sure the importHandler is always called, even if the user cancels the creation request.
         if newDocumentURL != nil {
-            importHandler(newDocumentURL, .move)
+            importHandler(newDocumentURL, .copy)
         } else {
             importHandler(nil, .none)
         }
