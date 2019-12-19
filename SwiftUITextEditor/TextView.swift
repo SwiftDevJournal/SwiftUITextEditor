609348febc7457531b9bc931af06cf33ca12e8ed
Mark Szymczyk <mark@meandmark.com>
2019-12-18 21:50:33 -0500

    Add a file for the text view for the text editor.

---
+++ SwiftUITextEditor/TextView.swift
@@ -0,0 +1,21 @@
+//
+//  TextView.swift
+//  SwiftUITextEditor
+//
+//  Created by mark on 12/18/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
